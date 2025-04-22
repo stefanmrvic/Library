ac326d94837cbe55f3e382b056ba086ec5eb0e36
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-04-22 22:41:47 +0200

    refactor: core logic

--- script/main.js
+++ script/main.js
@@ -1,9 +1,17 @@
 import '../styles/style.scss';
 
-const myLibrary = [];
+const myLibrary = [
+    {title: 'Na livadi divnoj', author: 'Lepi Stevdza', pages: 304, read: 'Have read it', id: '29083098-dlskdd-ws9923-sdsss'},
+    {title: 'Pevaj Golube', author: 'Dzigi', pages: 124, read: 'Didnt read it', id: '9839837-hjhh-sbsxb-97867'},
+    {title: 'Ko to tamo stenje', author: 'Savo Dzigljavi', pages: 324, read: 'Didnt read it', id: '829430-jhbmjm-huhhh-79987'},
+    {title: 'Egzotika', author: 'Miloje Slepi', pages: 24, read: 'Didnt read it', id: '8987678-jhjhh-88888-0000'},
+    {title: 'Mile prasuma', author: 'Dve Dangube', pages: 499, read: 'Have read it', id: 'kjhkjds-29883-dkjdfkj-22222'},
+    {title: 'Tvoja mama', author: 'Zoran Dzindzic', pages: 543, read: 'Have read it', id: '2222- sjhsjh-1111-0000'}
 
-function Book(name, author, pages, read) {
-    this.name = name;
+];
+
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
@@ -11,76 +19,75 @@ function Book(name, author, pages, read) {
 }
 
 function addBookToLibrary(event) {
-    const name = document.getElementById('name').value;
+    event.preventDefault();
+
+    const title = document.getElementById('name').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const read = document.getElementById('read').value;
     
-    const newBook = new Book(name, author, pages, read);
+    const newBook = new Book(title, author, pages, read);
     
     myLibrary.push(newBook);
     
-    addElement(name, author, pages, read, newBook.id);
-
-    event.preventDefault();
+    addElement(newBook);
+    form.reset();
 }
 
-function addElement(name, author, pages, read, id) {
+function addElement(book) {
     const parentContainer = document.querySelector('.library');
 
     const newLI = document.createElement('li');
     newLI.classList.add('library__item');
-    newLI.setAttribute('data-id', id);
+    newLI.setAttribute('data-id', book.id);
 
-    const newBtn = document.createElement('button');
-    newBtn.innerHTML = `
+    const newCloseBtn = document.createElement('button');
+    newCloseBtn.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
         <path d="M3 6h18v2H3V6zm2 3h14v13a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9zm3 2v9h2v-9H8zm4 0v9h2v-9h-2zm4 0v9h2v-9h-2z"/>
         </svg>
     `;
-    newBtn.classList.add('library__item__close-btn');
+    newCloseBtn.classList.add('library__item__close-btn');
 
-    const newH2 = document.createElement('h2');
-    newH2.textContent = name;
-    newH2.classList.add('library__item__title');
+    const newTitle = document.createElement('h2');
+    newTitle.textContent = book.title;
+    newTitle.classList.add('library__item__title');
 
-    const newPara1 = document.createElement('p');
-    newPara1.textContent = author;
-    newPara1.classList.add('library__item__author');
+    const newAuthor = document.createElement('p');
+    newAuthor.textContent = book.author;
+    newAuthor.classList.add('library__item__author');
 
-    const newPara2 = document.createElement('p');
-    newPara2.textContent = pages;
-    newPara2.classList.add('library__item__pages')
+    const newPages = document.createElement('p');
+    newPages.textContent = book.pages;
+    newPages.classList.add('library__item__pages')
 
-    const newPara3 = document.createElement('p');
-    newPara3.textContent = read;
-    newPara3.classList.add('library__item__read');
+    const newRead = document.createElement('p');
+    newRead.textContent = book.read;
+    newRead.classList.add('library__item__read');
 
     parentContainer.append(newLI);
-    newLI.append(newBtn, newH2, newPara1, newPara2, newPara3);
+    newLI.append(newCloseBtn, newTitle, newAuthor, newPages, newRead);
 }
 
+// === DOM SELECTORS === //
 const addBookBtn = document.querySelector('.add-book-btn');
 const closeBtn = document.querySelector('.close-btn');
-
-addBookBtn.addEventListener('click', () => {
-    const dialog = document.querySelector('.dialog');
-    dialog.showModal();
-});
-
-closeBtn.addEventListener('click', () => {
-    const dialog = document.querySelector('.dialog');
-    dialog.close();
-});
-
+const dialog = document.querySelector('.dialog');
 const form = document.querySelector('.form');
-form.addEventListener('submit', addBookToLibrary);
-
 const libraryContainer = document.querySelector('.library');
+
+// === EVENT BINDINGS === //
+addBookBtn.addEventListener('click', () => dialog.showModal());
+closeBtn.addEventListener('click', () => dialog.close());
+form.addEventListener('submit', addBookToLibrary);
 libraryContainer.addEventListener('click', deleteElement);
 
 function deleteElement(event) { 
+    const button = event.target.closest('button');
     const liElement = event.target.closest('.library__item');
+    
+    if (!button || !liElement) return;
+
     const liElementID = liElement.dataset.id;
 
     // Checks if the desired element for deletion exists in the myLibrary array & gets its index 
@@ -93,3 +100,28 @@ function deleteElement(event) {
         throw new Error('Element not found in an array!');
     }
 }
+
+function renderLibrary() {
+    const liBookElements = document.querySelectorAll('.library__item');
+
+    for (const obj of myLibrary) {
+        if (!obj.id) {
+            throw new Error(`Element in array doesn't have an ID!`);
+        }
+
+        const objectID = obj.id;
+        let newObject = true;
+
+        for (const element of liBookElements) {
+            if (element.dataset.id === objectID) {
+                newObject = false;
+            }
+        }
+
+        if (newObject) {
+            addElement(obj);
+        }
+    }
+}
+
+renderLibrary();
