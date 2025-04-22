c4617669076f2a8f7663d79d29162706fcf92483
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-04-22 17:18:22 +0200

    refactor: deleteElement() function logic

--- script/main.js
+++ script/main.js
@@ -18,25 +18,78 @@ function addBookToLibrary(event) {
     
     const newBook = new Book(name, author, pages, read);
     
-    // Put if statement that checks if the book already exists, to prevent duplicates
     myLibrary.push(newBook);
+    
+    addElement(name, author, pages, read, newBook.id);
+
     event.preventDefault();
 }
 
+function addElement(name, author, pages, read, id) {
+    const parentContainer = document.querySelector('.library');
+
+    const newLI = document.createElement('li');
+    newLI.classList.add('library__item');
+    newLI.setAttribute('data-id', id);
+
+    const newBtn = document.createElement('button');
+    newBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M3 6h18v2H3V6zm2 3h14v13a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9zm3 2v9h2v-9H8zm4 0v9h2v-9h-2zm4 0v9h2v-9h-2z"/>
+        </svg>
+    `;
+    newBtn.classList.add('library__item__close-btn');
+
+    const newH2 = document.createElement('h2');
+    newH2.textContent = name;
+    newH2.classList.add('library__item__title');
+
+    const newPara1 = document.createElement('p');
+    newPara1.textContent = author;
+    newPara1.classList.add('library__item__author');
+
+    const newPara2 = document.createElement('p');
+    newPara2.textContent = pages;
+    newPara2.classList.add('library__item__pages')
+
+    const newPara3 = document.createElement('p');
+    newPara3.textContent = read;
+    newPara3.classList.add('library__item__read');
+
+    parentContainer.append(newLI);
+    newLI.append(newBtn, newH2, newPara1, newPara2, newPara3);
+}
+
 const addBookBtn = document.querySelector('.add-book-btn');
+const closeBtn = document.querySelector('.close-btn');
 
 addBookBtn.addEventListener('click', () => {
     const dialog = document.querySelector('.dialog');
     dialog.showModal();
 });
 
+closeBtn.addEventListener('click', () => {
+    const dialog = document.querySelector('.dialog');
+    dialog.close();
+});
+
 const form = document.querySelector('.form');
 form.addEventListener('submit', addBookToLibrary);
 
+const libraryContainer = document.querySelector('.library');
+libraryContainer.addEventListener('click', deleteElement);
 
-// const test = new Book('Prvi put rokamo', 'Mrki', 235, 'procitaoo');
+function deleteElement(event) { 
+    const liElement = event.target.closest('.library__item');
+    const liElementID = liElement.dataset.id;
 
-// console.log(test);
+    // Checks if the desired element for deletion exists in the myLibrary array & gets its index 
+    const index = myLibrary.findIndex((book) => book.id === liElementID);
 
-//console.log(myLibrary);
-
+    if (index !== -1) {
+        myLibrary.splice(index, 1);
+        liElement.remove();
+    } else {
+        throw new Error('Element not found in an array!');
+    }
+}
